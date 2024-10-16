b3f0af8006a9a8213ceb4adf9537443cada09189
jkw1989 <jameskingwood89@gmail.com>
2024-10-16 12:35:17 -0700

    commit

--- Views/kpi_cards.py
+++ Views/kpi_cards.py
@@ -1,11 +1,25 @@
 import streamlit as st 
+from streamlit_extras.metric_cards import style_metric_cards
 
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+#with open('style.css') as f:
+#    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
 col1, col2, col3, col4, col5 = st.columns(5)
 
 col1.metric(label="My Metric!",value="75%",delta="-1,5%")
 col2.metric(label="My Metric!",value="50%",delta="1,5%")
-col3.metric(label="My Metric!",value="10%",delta="-10,5%")
+col3.metric(label="My Metric!",value="10%",delta="-10,5%",delta_color="off")
+
+
+col10, col11, col12 = st.columns(3)
+
+col10.metric(label="Gain", value=5000, delta=1000)
+col11.metric(label="Loss", value=5000, delta=-1000)
+col12.metric(label="No Change", value=5000, delta=0)
+
+style_metric_cards(
+    background_color="#E6E6E6",
+    box_shadow="False",
+    border_left_color="blue",
+)
--- Views/line_charts.py
+++ Views/line_charts.py
@@ -1,4 +1,25 @@
 import pandas as pd
 import streamlit as st
+import numpy as np 
+import plotly.express as px 
+from streamlit_extras.add_vertical_space import add_vertical_space
+
+
 
 st.title("Line Charts")
+
+df = pd.DataFrame(np.random.randn(10,2),columns = ["prices","diff"])
+col1, col2, col3 = st.columns(3)
+
+col1.line_chart(df)
+col2.line_chart(df,y="diff")
+
+add_vertical_space(2)
+
+st.divider()
+
+add_vertical_space(2)
+
+st.line_chart(df,y="diff",height=200,width=100)
+
+st.area_chart(df)
